05758beee8609b002688aedeef3c5fd3ad9ab5d2
Laiba Naveed <laibanaveed235@gmail.com>
2026-06-04 16:34:41 +0500

    Fix: patient-insurance route updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import patientInsuranceRoutes from './patient-insurance.routes';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import roleRoutes from './role.routes';
@@ -51,6 +52,7 @@ router.use('/roles', roleRoutes);
 router.use('/permissions', permissionRoutes);
 router.use('/practice-info', practiceInfoRoutes);
 router.use('/patients', patientRoutes);
+router.use('/patients', patientInsuranceRoutes);
 router.use('/insurance-companies', insuranceCompanyRoutes);
 router.use('/insurance-plans', insurancePlanRoutes);
 router.use('/coverage-templates', coverageTemplateRoutes);
